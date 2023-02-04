9b0ca11ff5ca9848f121bac28295e4d7cf44706d
Confused-Owp <Owlconfused@gmail.com>
2023-02-04 21:46:16 +0500

    Add variable to convert raw input to correct form

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
+// Machine chooses a random value within array
 const choiceOption = ['Rock', 'Paper', 'Scissor'];
 const random = Math.floor(Math.random() * choiceOption.length);
 console.log(choiceOption[random]);    
+
+// Human chooses random value 
+
+let playerInput = prompt('Enter Rock, Paper or Scissor').toLowerCase();
+console.log(playerInput);
+
+// Convert raw input into correct form
+let playerSelection =  playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
+console.log (playerSelection);
