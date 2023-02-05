6bad8e04f00d66294f1d369078712c209430d509
Confused-Owp <Owlconfused@gmail.com>
2023-02-06 04:54:39 +0500

    Make code more readable

--- game.js
+++ game.js
@@ -15,46 +15,40 @@ playerScore = 0 //initializing score of player
 function playRound(playerSelection , computerSelection){
   if ((playerSelection == 'Rock') || (playerSelection == 'Paper') || (playerSelection == 'Scissor')){
     if (playerSelection == computerSelection) {
-      alert('It is a draw!');
-      
-    } else if ((playerSelection  == 'Rock' && computerSelection == 'Scissor') ||
-      (playerSelection  == 'Paper' && computerSelection == 'Rock') ||
-      (playerSelection  == 'Scissor' && computerSelection == 'Paper'))
-            
-    {
-        alert (`you win! ${playerSelection} beats ${computerSelection}`)
-        playerScore++
-
-    }else {
+          alert('It is a draw!');  
+}   else if ((playerSelection  == 'Rock' && computerSelection == 'Scissor') ||
+            (playerSelection  == 'Paper' && computerSelection == 'Rock') ||
+            (playerSelection  == 'Scissor' && computerSelection == 'Paper'))
+{
+              alert (`you win! ${playerSelection} beats ${computerSelection}`)
+              playerScore++
+}   else {
         alert (`you have lost! ${computerSelection} beats ${playerSelection}`);
         computerScore++
-    }}else{
-      alert ('The input you have entered is invalid.')
-    }
-}
+}}else{
+    alert ('The input you have entered is invalid.')
+}}
 
 //finds out who wins the game
 let gameWinner= function (computerScore, playerScore) {
   if (computerScore > playerScore) {
     alert('The computer has won the game');
 
-  } else if (playerScore > computerScore) {
+} else if (playerScore > computerScore) {
     alert('The player has won the game');
 
-  } else {
+} else {
     alert('it was a draw!');
-  }
-};
+    
+}};
 
-//loop to make game into 5 rounds (need to convert into a function?)
+//loop to make game into 5 rounds
 function game(){
 for (let i = 0; i < 5; i++) {
   let playerValue = playerInput(prompt('Type Rock, Paper or Scissor') );
   let computerValue =getComputerChoice(['Rock','Paper', 'Scissor']);
   playRound(playerValue,computerValue)
-
 }
-
 gameWinner(computerScore,playerScore)
 }
 
