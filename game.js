f486b28d332fce51ba6882aed101735a1d56a895
Confused-Owp <Owlconfused@gmail.com>
2023-02-05 19:14:47 +0500

    Add comments and make code more readable

--- game.js
+++ game.js
@@ -7,14 +7,14 @@ let getComputerChoice = function (choiceOption) {
 //function to convert string into desired format
 let playerInput = (string) => string.toLowerCase() 
 
-computerScore = 0
-playerScore = 0
+computerScore = 0 //initializing score of computer
+playerScore = 0 //initializing score of player
+
 //function  for winning or losing
 function playRound(playerSelection , computerSelection){
 if (playerSelection == computerSelection) {
   alert('It is a draw!');
   
-
 } else if ((playerSelection  == 'rock' && computerSelection == 'scissor') ||
   (playerSelection  == 'paper' && computerSelection == 'rock') ||
   (playerSelection  == 'scissor' && computerSelection == 'paper'))
@@ -22,7 +22,6 @@ if (playerSelection == computerSelection) {
 {
       alert (`you win! ${playerSelection} beats ${computerSelection}`)
       playerScore++
-
 
 }else {
     alert (`you have lost! ${computerSelection} beats ${playerSelection}`);
@@ -42,12 +41,12 @@ let gameWinner= function (computerScore, playerScore) {
   }
 };
 
-
-
+//loop to make game into 5 rounds (need to convert into a function?)
 for (let i = 0; i < 5; i++) {
   let playerValue = playerInput(prompt('Type Rock, paper or Scissors') );
   let computerValue =getComputerChoice(['rock','paper', 'scissor']);
   playRound(playerValue,computerValue)
 
 }
+
 gameWinner(computerScore,playerScore)
