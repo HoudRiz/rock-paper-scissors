e6796f4e62ae231d112017316e9dbbb2752e8bc5
Confused-Owp <Owlconfused@gmail.com>
2023-02-05 09:43:12 +0500

    Make code more readable

--- game.js
+++ game.js
@@ -20,13 +20,16 @@ let playerValue = playerSelector(rawInput);
 //conditions for winning or losing
 if (playerValue == computerValue) {
   alert('It is a draw!');
+  
 } else if (playerValue  == 'rock' || computerValue == 'scissor') {
   alert (`you win! ${playerValue} beats ${computerValue}`)
 
 } else if (playerValue  == 'paper' || computerValue == 'rock') {
   alert (`you win! ${playerValue} beats ${computerValue}`)
+
 } else if (playerValue  == 'scissor' || computerValue == 'paper') {
   alert (`you win! ${playerValue} beats ${computerValue}`)
+
 }else {
   alert (`you have lost! ${computerValue} beats ${playerValue}`);
 };
