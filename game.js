d19425661e8ce7dd64c717c962ff25c1db6a199c
Confused-Owp <Owlconfused@gmail.com>
2023-02-05 14:22:01 +0500

    Simplify functions

--- game.js
+++ game.js
@@ -3,34 +3,32 @@ let getComputerChoice = function (choiceOption) {
   let random =  Math.floor(Math.random() * choiceOption.length);
   return (choiceOption[random]);    
 }
-const option = ['rock','paper', 'scissor'];
-let computerValue =getComputerChoice(option);
-console.log (computerValue)
+
+let computerValue =getComputerChoice(['rock','paper', 'scissor']);
+console.log (computerValue) //calling the function
   
 // Human chooses random value  Convert raw input into correct form
 
 let playerInput = function(string) {
     return string.toLowerCase() ;
-  }
-rawInput = prompt('Type Rock, paper or Scissors') 
-console.log(playerInput(rawInput))
-let playerValue = playerInput(rawInput);
-  
+  } 
+let playerValue = playerInput(prompt('Type Rock, paper or Scissors') );
+console.log (playerValue); // calling the function
 
 //conditions for winning or losing
 if (playerValue == computerValue) {
   alert('It is a draw!');
 
 } else if (playerValue  == 'rock' && computerValue == 'scissor') {
-  alert (`you win! ${playerValue} beats ${computerValue}`)
+   alert (`you win! ${playerValue} beats ${computerValue}`)
 
 } else if (playerValue  == 'paper' && computerValue == 'rock') {
-  alert (`you win! ${playerValue} beats ${computerValue}`)
+    alert (`you win! ${playerValue} beats ${computerValue}`)
 
 } else if (playerValue  == 'scissor' && computerValue == 'paper') {
-  alert (`you win! ${playerValue} beats ${computerValue}`)
+    alert (`you win! ${playerValue} beats ${computerValue}`)
 
 }else {
-  alert (`you have lost! ${computerValue} beats ${playerValue}`);
+    alert (`you have lost! ${computerValue} beats ${playerValue}`);
 };
 
