4871b56a42477a59822cccce2b35cee5b89b840f
Confused-Owp <Owlconfused@gmail.com>
2023-02-05 15:00:28 +0500

    Add counter (faulty)

--- game.js
+++ game.js
@@ -4,31 +4,37 @@ let getComputerChoice = function (choiceOption) {
   return (choiceOption[random]);    
 }
 
-
-  
-
 //function to convert string into desired format
 let playerInput = (string) => string.toLowerCase() 
 
-
-
+let lossCount = 0
+let winCount = 0
 //function  for winning or losing
-function playRound(playerSelection , computerSelection){
+function playRound(playerSelection , computerSelection,lossCount, winCount){
 if (playerSelection == computerSelection) {
   alert('It is a draw!');
 
 } else if (playerSelection  == 'rock' && computerSelection == 'scissor') {
    alert (`you win! ${playerSelection} beats ${computerSelection}`)
+   let win = winCount++
+   console.log(win)
 
 } else if (playerSelection  == 'paper' && computerSelection == 'rock') {
     alert (`you win! ${playerSelection} beats ${computerSelection}`)
+    let win = winCount++
+    console.log(win)
 
 } else if (playerSelection  == 'scissor' && computerSelection == 'paper') {
     alert (`you win! ${playerSelection} beats ${computerSelection}`)
+    let win = winCount++
+    console.log(win)
 
 }else {
     alert (`you have lost! ${computerSelection} beats ${playerSelection}`);
+    let loss = lossCount++
+    console.log(loss)
 };
+
 }
 
 
