1e868737d700b7e4da863e3b17fab872720ff72e
Confused-Owp <Owlconfused@gmail.com>
2023-02-05 09:35:21 +0500

    Add condition for loss and draw

--- game.js
+++ game.js
@@ -3,16 +3,26 @@ let computerSelector = function (choiceOption) {
   let random =  Math.floor(Math.random() * choiceOption.length);
   return (choiceOption[random]);    
 }
-
-console.log(computerSelector(['rock','paper', 'scissor']))
+const option = ['rock','paper', 'scissor'];
+let computerValue =computerSelector(option);
+console.log (computerValue)
   
 // Human chooses random value  Convert raw input into correct form
 
 let playerSelector = function(string) {
     return string.toLowerCase() ;
   }
+rawInput = prompt('Type Rock, paper or Scissors') 
+console.log(playerSelector(rawInput))
+let playerValue = playerSelector(rawInput);
   
-  console.log(playerSelector(prompt('Type Rock, paper or Scissors')))
 
 //conditions for winning or losing
+if (playerValue == computerValue) {
+  alert('It is a draw!');
+} else if (playerValue  == '' || computerValue == '') {
+  alert ('you win! ' + playerSelector  + computerSelector)
+} else {
+  alert ('you have lost!');
+};
 
