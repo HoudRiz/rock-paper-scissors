e4bbc2b807230c15a23c9ad51eab2e57c8a3504b
Confused-Owp <Owlconfused@gmail.com>
2023-02-05 16:17:38 +0500

    Remove counters

--- game.js
+++ game.js
@@ -10,29 +10,25 @@ let playerInput = (string) => string.toLowerCase()
 let lossCount = 0
 let winCount = 0
 //function  for winning or losing
-function playRound(playerSelection , computerSelection,lossCount, winCount){
+function playRound(playerSelection , computerSelection){
 if (playerSelection == computerSelection) {
   alert('It is a draw!');
 
 } else if (playerSelection  == 'rock' && computerSelection == 'scissor') {
    alert (`you win! ${playerSelection} beats ${computerSelection}`)
-   let win = winCount++
-   console.log(win)
 
 } else if (playerSelection  == 'paper' && computerSelection == 'rock') {
     alert (`you win! ${playerSelection} beats ${computerSelection}`)
-    let win = winCount++
-    console.log(win)
+
 
 } else if (playerSelection  == 'scissor' && computerSelection == 'paper') {
     alert (`you win! ${playerSelection} beats ${computerSelection}`)
-    let win = winCount++
-    console.log(win)
+
+  
 
 }else {
     alert (`you have lost! ${computerSelection} beats ${playerSelection}`);
-    let loss = lossCount++
-    console.log(loss)
+
 };
 
 }
